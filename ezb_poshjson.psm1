87b8738aaaac3b3ebe9941283478b9948f07c65f
renaud Devers <rdevers@chavers.org>
2018-10-12 18:40:42 +0200

    update psm1

--- ezb_poshjson.psm1
+++ ezb_poshjson.psm1
@@ -2,17 +2,9 @@
 if(!$PSScriptRoot) {
     $PSScriptRoot = $MyInovocation.PSScriptRoot
 }
-# $Global:moduleroot = $MyInovocation.PSScriptRoot
+
 $Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
-if ([Type]::GetType("Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver") -eq $Null) {
-    if ($PSVersionTable.PSEdition -eq "Core") {
-        [System.Reflection.Assembly]::LoadFile("${PSScriptRoot}\lib\netstandard1.3\Newtonsoft.Json.dll")
-    }
-    else {
-        [System.Reflection.Assembly]::LoadFile("${PSScriptRoot}\lib\net45\Newtonsoft.Json.dll")
-    }
-}
 Foreach($import in @($Public + $Private))
 {
     Try
@@ -29,3 +21,13 @@ Foreach($import in @($Public + $Private))
 
 
 Export-ModuleMember -Function $Public.Basename
+
+
+if ([Type]::GetType("Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver") -eq $Null) {
+    if ($PSVersionTable.PSEdition -eq "Core") {
+        [System.Reflection.Assembly]::LoadFile("${PSScriptRoot}\lib\netstandard1.3\Newtonsoft.Json.dll")
+    }
+    else {
+        [System.Reflection.Assembly]::LoadFile("${PSScriptRoot}\lib\net45\Newtonsoft.Json.dll")
+    }
+}
