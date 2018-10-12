67a98df32f8225b1fbdaa22e29ab18d594041835
renaud Devers <rdevers@chavers.org>
2018-10-12 18:41:24 +0200

    update private

--- private/Write-ObjectToNewtonsoftFriendlyValue.ps1
+++ private/Write-ObjectToNewtonsoftFriendlyValue.ps1
@@ -1,49 +1,46 @@
 function Write-ObjectToNewtonsoftFriendlyValue() {
     Param(
         [Parameter(Position = 0)]
-        [Object] $InputObject 
+        $InputObject 
     )
-   
-    if ($InputObject -eq $null) {
+   #[Object] $InputObject 
+
+   if ($InputObject -eq $null) {
         return $null;
     }
-    switch ($InputObject.GetType().Name) {
-        string { return [string] $InputObject }
-        Array {
-            $result = @()
-            foreach ($item in $InputObject) {
-                $result += Write-ObjectToNewtonsoftFriendlyValue -InputObject $item 
-            }
-            return $result; 
-        }
-        hashtable {
-            $dictionary = New-Object "System.Collections.Generic.Dictionary[[string],[Object]]"
-            foreach ($key in $InputObject.Keys) {
-                $value = Write-ObjectToNewtonsoftFriendlyValue -InputObject $InputObject[$key] 
-                $dictionary.Add($key, $value)
-            }
-            return $dictionary 
-        }
-        psobject {
-            $dictionary = New-Object "System.Collections.Generic.Dictionary[[string],[Object]]"
-            $InputObject | Get-Member -MemberType Properties | Foreach-Object {
-                $name = $_.Name 
-                $value = Write-ObjectToNewtonsoftFriendlyValue  -InputObject ($InputObject.$Name)
-                $dictionary.Add($name, $value)
-            }
+    if($InputObject -is [Array]) {
+		Write-Verbose "is Array"
+	    $result =  @()
+	    foreach($item in $InputObject) {
+	        $result += Write-ObjectToNewtonsoftFriendlyValue -InputObject $item 
+	    }
+	    return $result;
+    } 
 
-            return $dictionary; 
+    if($InputObject -is [hashtable]) {
+       Write-Verbose "is hashtable"
+        #$dictionary = New-Object "System.Collections.Generic.Dictionary[[string],[Object]]"
+		$dictionary = @{}
+        foreach($key in $InputObject.Keys) {
+            $value = Write-ObjectToNewtonsoftFriendlyValue -InputObject $InputObject[$key] 
+            $dictionary.Add($key, $value)
         }
-        PSCustomObject {
-            $dictionary = New-Object "System.Collections.Generic.Dictionary[[string],[Object]]"
-            $InputObject | Get-Member -MemberType Properties | Foreach-Object {
-                $name = $_.Name 
-                $value = Write-ObjectToNewtonsoftFriendlyValue  -InputObject ($InputObject.$Name)
-                $dictionary.Add($name, $value)
-            }
-
-            return $dictionary; 
-        }
-        Default {return $InputObject; }
+        return $dictionary
     }
+
+    if($InputObject -is [psobject]) {
+       Write-Verbose "is psobject"
+        #$dictionary = New-Object "System.Collections.Generic.Dictionary[[string],[Object]]"
+		$dictionary = @{}
+         $InputObject | Get-Member -MemberType Properties | Foreach-Object {
+            $name = $_.Name 
+            $value = Write-ObjectToNewtonsoftFriendlyValue  -InputObject ($InputObject.$name)
+            $dictionary.Add($name, $value)
+         }
+
+         return $dictionary;
+    }
+
+    return $InputObject;
+	
 }
